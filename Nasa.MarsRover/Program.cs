3de9018dd66dc104ba2895c1f467dbc71b0c3b91
hylmz <ylmzhsn94@gmail.com>
2020-02-21 23:27:30 +0300

    Implement below changes; CommandPattern SOLID UnitTests

---
+++ Nasa.MarsRover/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+using Autofac;
+
+namespace Nasa.MarsRover
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var commandString = BuildCommandString();
+            var containerBuilder = createContainerBuilder();
+
+            using (var container = containerBuilder.Build())
+            {
+                var roverCommandCenter = container.Resolve<ICommandCenter>();
+                roverCommandCenter.Execute(commandString);
+                var roverReports = roverCommandCenter.GetCombinedRoverReport();
+                DisplayToConsole(commandString, roverReports);
+            }
+        }
+
+        private static ContainerBuilder createContainerBuilder()
+        {
+            var programAssembly = Assembly.GetExecutingAssembly();
+
+            var builder = new ContainerBuilder();
+            builder.RegisterAssemblyTypes(programAssembly)
+                .AsImplementedInterfaces();
+
+            return builder;
+        }
+
+        private static void DisplayToConsole(string commandString, string roverReports)
+        {
+            Console.WriteLine("Input:");
+            Console.WriteLine(commandString);
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Output:");
+            Console.WriteLine(roverReports);
+            Console.Write(Environment.NewLine);
+            Console.Write("Press <enter> to exit...");
+            Console.ReadLine();
+        }
+
+        private static string BuildCommandString()
+        {
+            var commandStringBuilder = new StringBuilder();
+            commandStringBuilder.AppendLine("5 5");
+            commandStringBuilder.AppendLine("1 2 N");
+            commandStringBuilder.AppendLine("LMLMLMLMM");
+            commandStringBuilder.AppendLine("3 3 E");
+            commandStringBuilder.Append("MMRMMRMRRM");
+            return commandStringBuilder.ToString();
+        }
+    }
+}
